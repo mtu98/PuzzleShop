63002dfaa566206d0eec083e8cb20b510e352f78
mtu98 <mitu9811@gmail.com>
2018-07-01 14:37:13 +0700

    Model
    App.config, connection, Account collection operating sample

--- SampleData/Data.bat
+++ SampleData/Data.bat
@@ -1,2 +1,3 @@
+@echo off
 mongo PuzzleShopDB.js
 pause
---
+++ SampleData/PuzzleShopModel/Library/Account/AccountDAO.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using MongoDB.Driver;
+using MongoDB.Bson;
+
+namespace Library.Account
+{
+    public class AccountDAO
+    {
+        /// <summary>
+        /// Check login function
+        /// </summary>
+        /// <param name="username"></param>
+        /// <param name="password"></param>
+        /// <returns></returns>
+        public bool checkLogin(string username, string password)
+        {
+            //Get connection and PuzzleShopDB
+            var db = utils.DBConnect.getDB();
+            var accounts = db.GetCollection<AccountDTO>("Account");
+            var builder = Builders<AccountDTO>.Filter;
+            var filter = builder.Where(a => a.username.Equals(username));
+            List<AccountDTO> list = accounts.Find(filter).ToList();
+            if(list.Count > 0)
+            {
+                string passwordHash = list[0].passwordHash;
+                if (MD5Hash.VerifyMD5Hash(password, passwordHash))
+                    return true;
+            }
+            return false;
+        }
+
+        public void register(string username, string password, string firstName, string lastName, string email)
+        {
+            var db = utils.DBConnect.getDB();
+            var accounts = db.GetCollection<AccountDTO>("Account");
+            string passwordHash = MD5Hash.GetMD5Hash(password);
+            accounts.InsertOneAsync(new AccountDTO
+            {
+                username = username,
+                passwordHash = passwordHash,
+                firstName = firstName,
+                lastName = lastName,
+                email = email,
+                role = 1
+            });
+        }
+
+    }
+}
