7e5f8fb4904d6e4fe5b36999812da3a754b84a07
ThanhNhan776 <thanhnhan.toantin@gmail.com>
2018-07-22 22:29:08 +0700

    View Order Detail - 95% complete overall

---
+++ App/PuzzleShop/PuzzleShop.Client/Pages/ViewOrder/Logic/ViewAnOrderLogic.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Blazor.Components;
+using PuzzleShop.Shared.Models.Order;
+using System.Diagnostics;
+
+namespace PuzzleShop.Client.Pages.ViewOrder.Logic {
+    public class ViewAnOrderLogic : BlazorComponent {
+        [Parameter]
+        protected Orders Order { get; set; }
+
+        [Parameter]
+        protected int OrderNumber { get; set; }
+
+        protected override void OnParametersSet() {
+            if (Order != null) {
+                Debug.WriteLine("==> parameter: order index: " + OrderNumber + ", order date: " + Order.OrderDate);
+            }
+            StateHasChanged();
+        }
+    }
+}
